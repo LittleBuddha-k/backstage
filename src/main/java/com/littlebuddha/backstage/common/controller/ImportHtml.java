0038ed9970b2d86372001a25a57218c0f44b8f30
LittleBuddha-k <2998986128@qq.com>
2020-12-09 17:24:02 +0800

    tnm

--- src/main/java/com/littlebuddha/backstage/common/controller/ImportHtml.java
+++ src/main/java/com/littlebuddha/backstage/common/controller/ImportHtml.java
@@ -2,12 +2,14 @@ package com.littlebuddha.backstage.common.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author ck
  * @date 2020/12/9 15:00
  */
 @Controller
+@RequestMapping("/common")
 public class ImportHtml {
 
     @GetMapping("/importHtml")

1c3211b06e51f7a019151fa2090904fed63a8062
LittleBuddha-k <2998986128@qq.com>
2021-02-23 14:46:54 +0800

    注解掉swagger功能

--- src/main/java/com/littlebuddha/backstage/common/config/swagger/SwaggerConfig.java
+++ src/main/java/com/littlebuddha/backstage/common/config/swagger/SwaggerConfig.java
@@ -8,7 +8,6 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * swagger配置类
@@ -16,7 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * @date 2021/2/23 10:35
  */
 @Configuration
-@EnableSwagger2
+/*@EnableSwagger2*/
 public class SwaggerConfig {
     @Bean
     public Docket createRestApi(){

2ff5e8c4946294edb2f07552b9da17eb96d23cb2
LittleBuddha-k <2998986128@qq.com>
2021-02-25 16:12:13 +0800

    集成swagger

--- src/main/java/com/littlebuddha/backstage/common/config/swagger/SwaggerConfig.java
+++ src/main/java/com/littlebuddha/backstage/common/config/swagger/SwaggerConfig.java
@@ -1,13 +1,20 @@
 package com.littlebuddha.backstage.common.config.swagger;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 /**
  * swagger配置类
@@ -15,21 +22,41 @@ import springfox.documentation.spring.web.plugins.Docket;
  * @date 2021/2/23 10:35
  */
 @Configuration
-/*@EnableSwagger2*/
+@EnableOpenApi
 public class SwaggerConfig {
-    @Bean
-    public Docket createRestApi(){
-        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any()).build();
-    }
+    /**
+     * 是否开启swagger配置，生产环境需关闭
+     */
+    @Value("${swagger.enabled}")
+    private boolean enable;
 
-    private ApiInfo apiInfo(){
-        return new ApiInfoBuilder()
-                .title("SpringBoot API Doc")
-                .description("This is a restful api document of Spring Boot.")
-                .version("1.0")
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.OAS_30).pathMapping("/")
+                .enable(enable)
+                .apiInfo(this.apiInfo())
+                .select() // 指定需要发布到Swagger的接口目录，不支持通配符
+                .apis(RequestHandlerSelectors.basePackage("com.littlebuddha.backstage.modules.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                // 支持的通讯协议集合
+                .protocols(this.newHashSet("https", "http"));
+    }
+    /**
+     * 项目信息
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Swagger Api Doc")
+                .description("SpringBoot后台接口")
+                .contact(new Contact("user", null, "xyz@qq.com"))
+                .version("Application Version: 1.0.0")
                 .build();
+    }
+    @SafeVarargs
+    private final <T> Set<T> newHashSet(T... ts) {
+        if (ts.length > 0) {
+            return new LinkedHashSet<>(Arrays.asList(ts));
+        }
+        return null;
     }
 }
